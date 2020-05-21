d65b244871169cec4df0e90703183c5cfa3bf9fb
alexandr <flex.nst@gmail.com>
2020-05-22 00:52:05 +0700

    Runner register manual easy

--- runner-register.sh
+++ runner-register.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-docker exec -it runner gitlab-runner register
+docker exec -it runner rm -rf /etc/gitlab-runner/config.toml
+docker exec -it runner gitlab-runner register \
+  --docker-network-mode="gitlab-workflow_default" \
+  --description='CI-runner' \
+  --url="http://gitlab" \
+  --clone-url="http://gitlab" \
+  --executor="docker" \
+  --docker-image="docker" \
+  --locked='false' \
+  --paused='false' \
+  --tag-list='default,runner'
