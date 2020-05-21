060809189a463975ba7b83413a66d97959182418
alexandr <flex.nst@gmail.com>
2020-05-22 00:08:24 +0700

    Runner register script

--- register-runner.sh
+++ register-runner.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-
-docker run --rm -it \
-  -v $(pwd)/services/runner/config:/etc/gitlab-runner \
-  --network gitlab-workflow_default \
-  gitlab/gitlab-runner register \
-#  --non-interactive \
-  --executor "docker" \
-  --docker-image alpine:latest \
-  --url "http://gitlab" \
-  --registration-token "$0" \
-  --description "docker-runner" \
-  --tag-list "default-runner" \
-  --run-untagged="true" \
-  --locked="false" \
-  --access-level="not_protected"
+docker cp ./services/runner/config/config.toml runner:/etc/gitlab-runner/config.toml
+docker exec -it runner sed -i "s/REGISTRATION_TOKEN/$1/g" /etc/gitlab-runner/config.toml
+docker exec -it runner gitlab-runner restart
