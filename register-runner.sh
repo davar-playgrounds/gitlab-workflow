29cdbbdb962054b9f91c5a8a5c7c5b64fc36c21b
alexandr <flex.nst@gmail.com>
2020-05-22 00:14:35 +0700

    Runner register script

--- register-runner.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker cp ./services/runner/config/config.toml runner:/etc/gitlab-runner/config.toml
-docker exec -it runner sed -i "s/REGISTRATION_TOKEN/$1/g" /etc/gitlab-runner/config.toml
-docker exec -it runner gitlab-runner restart
---
+++ runner-register.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker exec -it runner gitlab-runner register
